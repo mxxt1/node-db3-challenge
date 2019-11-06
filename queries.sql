e6a085f8dba2004ac23218be7fe29823e9488517
mxxt1 <matt@dustcloudlabs.com>
2019-11-06 15:39:02 -0500

    init, first two queries

--- queries.sql
+++ queries.sql
@@ -2,7 +2,15 @@
 
 -- Display the ProductName and CategoryName for all products in the database. Shows 77 records.
 
+SELECT p.ProductName, c.categoryName FROM Product AS "p" JOIN Category AS "c" ON p.CategoryId = c.Id 
+
+
+
 -- Display the order Id and shipper CompanyName for all orders placed before August 9 2012. Shows 429 records.
+
+SELECT o.id, s.CompanyName FROM [Order] AS 'o' JOIN Shipper AS 's' ON o.ShipVia = s.ID WHERE o.OrderDate < '2012-08-09';
+
+
 
 -- Display the name and quantity of the products ordered in order with Id 10251. Sort by ProductName. Shows 3 records.
 
