3d241f19381e22f5abbd1950d4db982f2bc6ef1a
mxxt1 <matt@dustcloudlabs.com>
2019-11-06 16:57:30 -0500

    MVP

--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -3,6 +3,11 @@ const db = require('../data/dbConfig.js');
 module.exports = {
     find,
     findById,
+    findSteps,
+    add,
+    addStep,
+    remove,
+    update,
 };
 
 //find
@@ -22,13 +27,33 @@ function findById(id){
 
 //findSteps(id)
 
-
+function findSteps(id){
+    const scheme_id = id;
+    return db('steps').where({scheme_id}).orderBy('step_number', 'ASC');
+}
 
 //add(scheme)
+
+function add(scheme){
+    return db('schemes').insert(scheme);
+    
+}
+
+function addStep(data, id){
+    return db('steps').insert(data)
+}
 
 
 //update(changes, id)
 
+function update(changes, id){
+    return db('schemes').where({id}).update(changes);
+}
+
 
 //remove(id)
+
+function remove(id){
+    return db('schemes').where({id}).delete();
+}
 
